0713911776cdbb08ca5f0b2fc88a71f7f1772695
cremarco <macre04@gmail.com>
2016-04-14 18:58:34 +0200

    Inzio scrittura query

--- src/Umbria/TelegramBotBundle/UpdateReceiver/UpdateReceiver.php
+++ src/Umbria/TelegramBotBundle/UpdateReceiver/UpdateReceiver.php
@@ -2,12 +2,23 @@
 
 namespace Umbria\TelegramBotBundle\UpdateReceiver;
 
+use AnthonyMartin\GeoLocation\GeoLocation;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query\Parameter;
 use Shaygan\TelegramBotApiBundle\TelegramBotApi;
 use Shaygan\TelegramBotApiBundle\Type\Update;
+use JMS\DiExtraBundle\Annotation as DI;
+use Doctrine\DBAL\Types\Type;
 use TelegramBot\Api\Types\ReplyKeyboardMarkup;
 
 class UpdateReceiver implements UpdateReceiverInterface
 {
+    /**
+     * @var EntityManager
+     * @DI\Inject("doctrine.orm.entity_manager")
+     */
+    public $em;
 
     private $config;
     private $telegramBotApi;
@@ -30,7 +41,7 @@ class UpdateReceiver implements UpdateReceiverInterface
         $newKeyboard = json_encode($newArray, true);*/
 
         // Controllo se all'interno dell'Umbria
-        if(isset($message['location'])) {
+        if (isset($message['location'])) {
             if (($message['location']['latitude'] >= 45 AND $message['location']['latitude'] <= 45.7)
                 AND ($message['location']['longitude'] >= 9 AND $message['location']['longitude'] <= 9.5)
             ) {
@@ -42,13 +53,13 @@ class UpdateReceiver implements UpdateReceiverInterface
             $this->telegramBotApi->sendMessage($message['chat']['id'], $text);
         }
 
-        if(isset($message['text'])) {
+        if (isset($message['text'])) {
             switch ($message['text']) {
                 case "/about":
                     $text = "UmbriaTourismBot ti permette di ricevere informazioni turistiche. Invia la tua posizione per scoprire tutte le bellezze che la nostra regione ha in serbo per te";
                     break;
                 case "/hello":
-                    $text = "Ciao ". $message['from']['first_name'].". Oggi ti consiglio di visitare la Gola del Bottaccione";
+                    $text = "Ciao " . $message['from']['first_name'] . ". Oggi ti consiglio di visitare la Gola del Bottaccione";
                     break;
                 case "/help":
                 case "/start":
@@ -68,4 +79,48 @@ class UpdateReceiver implements UpdateReceiverInterface
         }
 
     }
+
+    public function createQuery($lat, $lng)
+    {
+        $builder = $this->em->createQueryBuilder()
+            ->select('s')
+            ->from('UmbriaOpenApiBundle:Tourism\Attractor', 's');
+
+        if ($lat && $lng) {
+            $lat = floatval($lat);
+            $lng = floatval($lng);
+            $radius = 10;
+
+            $lat = floatval($lat);
+            $lng = floatval($lng);
+            $radius = floatval($radius);
+
+            $location = GeoLocation::fromDegrees($lat, $lng);
+            /** @var GeoLocation[] $bounds */
+            $bounds = $location->boundingCoordinates($radius, 'km');
+
+            // AS HIDDEN consente di definire un campo di ordinamento (usabile in having) senza alterare
+            // il risultato restituito né il formato di restituzione!
+            $alias = 'HIDDEN distance';
+            $builder->select("s, GEO_DISTANCE(:lat, :lng, s.latitude, s.longitude) AS $alias")
+                ->andWhere('s.latitude BETWEEN :minLat and :maxLat')
+                ->andWhere('s.longitude BETWEEN :minLng and :maxLng')
+                ->andWhere('GEO_DISTANCE(:lat, :lng, s.latitude, s.longitude) < :radius')
+                ->orderBy('distance');
+
+            // è necessario specificare i tipi dei parametri come INTEGER per evitare che doctrine
+            // inserisca gli apici, cosa che impedisce il funzionamento della query.
+            $builder->setParameters(new ArrayCollection(array(
+                new Parameter('lat', $lat, Type::INTEGER),
+                new Parameter('lng', $lng, Type::INTEGER),
+                new Parameter('minLat', $bounds[0]->getLatitudeInDegrees(), Type::INTEGER),
+                new Parameter('minLng', $bounds[0]->getLongitudeInDegrees(), Type::INTEGER),
+                new Parameter('maxLat', $bounds[1]->getLatitudeInDegrees(), Type::INTEGER),
+                new Parameter('maxLng', $bounds[1]->getLongitudeInDegrees(), Type::INTEGER),
+                new Parameter('radius', $radius, Type::INTEGER),
+            )));
+
+        }
+    }
+
 }
