a3b4c1f806d68c46813d2a6a9a6b37e399d32b23
Lorenzo Ranucci <loryzizu@gmail.com>
2016-06-07 10:23:34 +0200

    Added parameters

--- src/Umbria/OpenApiBundle/Controller/Tourism/EventController.php
+++ src/Umbria/OpenApiBundle/Controller/Tourism/EventController.php
@@ -83,7 +83,14 @@ class EventController extends FOSRestController
      *  },
      *  parameters={
      *      {"name"="start", "dataType"="integer", "required"=false, "description"="Elemento da cui partire"},
-     *      {"name"="limit", "dataType"="integer", "required"=false, "description"="Numero di elementi restituiti"}
+     *      {"name"="limit", "dataType"="integer", "required"=false, "description"="Numero di elementi restituiti"},
+     *      {"name"="title_like", "dataType"="string", "required"=false, "description"="Condizione 'LIKE' su denominazione"},
+     *      {"name"="descriptions_like", "dataType"="string", "required"=false, "description"="Condizione 'LIKE' sulle descrizioni"},
+     *      {"name"="category_like", "dataType"="string", "required"=false, "description"="Condizione 'LIKE' sulle categorie"},
+     *      {"name"="lat_max", "dataType"="number", "required"=false, "description"="Latitudine massima"},
+     *      {"name"="lat_min", "dataType"="number", "required"=false, "description"="Latitudine minima"},
+     *      {"name"="lng_max", "dataType"="number", "required"=false, "description"="Longitudine massima"},
+     *      {"name"="lng_min", "dataType"="number", "required"=false, "description"="Longitudine minima"}
      *  },
      *  statusCodes={
      *      200="Returned when successful"
@@ -99,6 +106,13 @@ class EventController extends FOSRestController
         $filters = $this->filterBag->getFilterBag($request);
         $offset = $filters->has('start') ? $filters->get('start') : 0;
         $limit = $filters->has('limit') ? $filters->get('limit') : self::DEFAULT_PAGE_SIZE;
+        $labelLike = $filters->has('title_like') ? $filters->get('title_like') : null;
+        $descriptionLike = $filters->has('descriptions_like') ? $filters->get('descriptions_like') : null;
+        $categoryLike = $filters->has('category_like') ? $filters->get('category_like') : null;
+        $latMax = $filters->has('lat_max') && $filters->get('lat_max') ? floatval($filters->get('lat_max')) : null;
+        $latMin = $filters->has('lat_min') && $filters->get('lat_min') ? floatval($filters->get('lat_min')) : null;
+        $lngMax = $filters->has('lng_max') && $filters->get('lng_max') ? floatval($filters->get('lng_max')) : null;
+        $lngMin = $filters->has('lng_min') && $filters->get('lng_min') ? floatval($filters->get('lng_min')) : null;
         if ($limit == 0) {
             $limit = self::DEFAULT_PAGE_SIZE;
         }
@@ -127,9 +141,90 @@ class EventController extends FOSRestController
             $this->curlBuilder->updateEntities($url, self::DATASET_TOURISM_EVENT, $urlSilkSameAs);
         }
 
-        $builder = $this->em->createQueryBuilder()
+        $qb = $this->em->createQueryBuilder();
+        $builder = $qb
             ->select('a')
             ->from('UmbriaOpenApiBundle:Tourism\Event', 'a');
+
+        if ($labelLike != null) {
+            $builder = $qb
+                ->andWhere($qb->expr()->like('a.title', '?2'))
+                ->setParameter(2, $labelLike);
+        }
+
+        if ($descriptionLike != null) {
+            $builder = $qb
+                ->innerJoin('a.descrizioni', 'd')
+                ->andWhere(
+                    $qb->expr()->orX(
+                        $qb->expr()->like('d.testo', '?1'),
+                        $qb->expr()->like('d.titolo', '?1'),
+                        $qb->expr()->like('a.eventDescription', '?1')
+                    )
+                )
+                ->setParameter(1, $descriptionLike);
+
+        }
+
+        if ($categoryLike != null) {
+            $builder = $qb
+                ->leftJoin('a.categorie', 'cat')
+                ->andWhere(
+                    $qb->expr()->like('cat.cat', ':categoryLike')
+                )
+                ->setParameter("categoryLike", $categoryLike);
+
+        }
+
+        if ($latMax != null ||
+            $latMin != null ||
+            $lngMax != null ||
+            $lngMin != null
+        ) {
+            $builder = $qb
+                ->innerJoin('a.coordinate', 'c');
+            if ($latMax != null) {
+                $builder =
+                    $qb->andWhere(
+                        $qb->expr()->lte("c.latitude", ':latMax'),
+                        $qb->expr()->isNotNull("c.latitude"),
+                        $qb->expr()->gt("c.latitude", ':empty')
+                    )
+                        ->setParameter('latMax', $latMax)
+                        ->setParameter('empty', '0');
+            }
+            if ($latMin != null) {
+                $builder =
+                    $qb->andWhere(
+                        $qb->expr()->gte("c.latitude", ':latMin'),
+                        $qb->expr()->isNotNull("c.latitude"),
+                        $qb->expr()->gt("c.latitude", ":empty")
+                    )
+                        ->setParameter('latMin', $latMin)
+                        ->setParameter('empty', '0');
+            }
+            if ($lngMax != null) {
+                $builder =
+                    $qb->andWhere(
+                        $qb->expr()->lte("c.longitude", ':lngMax'),
+                        $qb->expr()->isNotNull("c.longitude"),
+                        $qb->expr()->gt("c.longitude", ":empty")
+                    )
+                        ->setParameter('lngMax', $lngMax)
+                        ->setParameter('empty', '0');
+            }
+            if ($lngMin != null) {
+                $builder =
+                    $qb->andWhere(
+                        $qb->expr()->gte("c.longitude", ':lngMin'),
+                        $qb->expr()->isNotNull("c.longitude"),
+                        $qb->expr()->gt("c.longitude", ":empty")
+                    )
+                        ->setParameter('lngMin', $lngMin)
+                        ->setParameter('empty', '0');
+            }
+        }
+
 
         /** @var AbstractPagination $resultsPagination */
         $resultsPagination = $this->paginator->paginate($builder, $page, $limit);
