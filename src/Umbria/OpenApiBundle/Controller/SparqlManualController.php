6fb392fea22debfc178c90c7ee410f9c9b35d405
Lorenzo Ranucci <loryzizu@gmail.com>
2016-11-14 12:59:22 +0100

    sparql manual query from production sparql endpoint

--- src/Umbria/OpenApiBundle/Controller/SparqlManualController.php
+++ src/Umbria/OpenApiBundle/Controller/SparqlManualController.php
@@ -26,21 +26,21 @@ class SparqlManualController extends Controller
 
     public function sparqlQueryGraphsAction()
     {
-        $url = "https://odnt-srv01/sparql?default-graph-uri=http%3A%2F%2Fdati.umbria.it%2Fgraph%2Fattrattor&query=SELECT+DISTINCT+%3Fg%0D%0AWHERE%7B%0D%0A++++GRAPH+%3Fg+%7B%3Fs+a+%3Ft%7D%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on";
+        $url = "http://dati.umbria.it/sparql?default-graph-uri=http%3A%2F%2Fdati.umbria.it%2Fgraph%2Fattrattor&query=SELECT+DISTINCT+%3Fg%0D%0AWHERE%7B%0D%0A++++GRAPH+%3Fg+%7B%3Fs+a+%3Ft%7D%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on";
         $response = $this->getResource($url);
         return new JsonResponse(array('data' => $response));
     }
 
     public function sparqlQueryTypesAction($graph)
     {
-        $url = "https://odnt-srv01/sparql?default-graph-uri=" . $graph . "&query=SELECT+DISTINCT+%3Fo%0D%0AWHERE%7B%0D%0A++++%3Fs+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23type%3E+%3Fo%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on";
+        $url = "http://dati.umbria.it/sparql?default-graph-uri=" . $graph . "&query=SELECT+DISTINCT+%3Fo%0D%0AWHERE%7B%0D%0A++++%3Fs+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23type%3E+%3Fo%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on";
         $response = $this->getResource($url);
         return new JsonResponse(array('data' => $response));
     }
 
     public function sparqlQueryDataAction($graph, $type)
     {
-        $url = "https://odnt-srv01/sparql?default-graph-uri=" . $graph . "&query=SELECT+DISTINCT+%3Fs+%3Fp+%3Fo+WHERE%7B+%3Fs+%3Fp+%3Fo+.+%3Fs+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23type%3E+%3C" . $type . "%3E+%7DLIMIT+50&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on";
+        $url = "http://dati.umbria.it/sparql?default-graph-uri=" . $graph . "&query=SELECT+DISTINCT+%3Fs+%3Fp+%3Fo+WHERE%7B+%3Fs+%3Fp+%3Fo+.+%3Fs+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23type%3E+%3C" . $type . "%3E+%7DLIMIT+50&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on";
         $response = $this->getResource($url);
         return new JsonResponse(array('data' => $response));
     }
