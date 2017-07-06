9babefa533293a257fd34ae96e57071218f7c0c8
LukeChan <lukechan170125@Lukede-MacBook-Pro.local>
2017-07-06 15:07:40 +0200

    missed vendor added

---
+++ vendor/craue/geo-bundle/Tests/AppKernel.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Craue\GeoBundle\Tests;
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\Kernel;
+
+class AppKernel extends Kernel {
+
+	private $configFiles;
+
+	public function __construct($environment, $configFiles) {
+		parent::__construct($environment, true);
+
+		if (!is_array($configFiles)) {
+			$configFiles = (array) $configFiles;
+		}
+
+		$this->configFiles = array();
+
+		foreach ($configFiles as $configFile) {
+			$fs = new Filesystem();
+			if (!$fs->isAbsolutePath($configFile)) {
+				$configFile = __DIR__ . '/config/' . $configFile;
+			}
+
+			if (!file_exists($configFile)) {
+				throw new \RuntimeException(sprintf('The config file "%s" does not exist.', $configFile));
+			}
+
+			$this->configFiles[] = $configFile;
+		}
+	}
+
+	public function registerBundles() {
+		return array(
+			new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+			new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+			new \Craue\GeoBundle\CraueGeoBundle(),
+		);
+	}
+
+	public function registerContainerConfiguration(LoaderInterface $loader) {
+		if (!is_array($this->configFiles)) {
+			$this->configFiles = (array) $this->configFiles;
+		}
+
+		foreach ($this->configFiles as $configFile) {
+			$loader->load($configFile);
+		}
+	}
+
+	public function getCacheDir() {
+		if (array_key_exists('CACHE_DIR', $_ENV)) {
+			return $_ENV['CACHE_DIR'] . DIRECTORY_SEPARATOR . $this->environment;
+		}
+
+		return parent::getCacheDir();
+	}
+
+	public function getLogDir() {
+		if (array_key_exists('LOG_DIR', $_ENV)) {
+			return $_ENV['LOG_DIR'] . DIRECTORY_SEPARATOR . $this->environment;
+		}
+
+		return parent::getLogDir();
+	}
+
+	public function serialize() {
+		return serialize(array($this->environment, $this->configFiles));
+	}
+
+	public function unserialize($data) {
+		list($environment, $configFiles) = unserialize($data);
+		$this->__construct($environment, $configFiles);
+	}
+
+}
