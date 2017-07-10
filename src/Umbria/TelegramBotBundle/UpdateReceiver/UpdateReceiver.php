02a720e40c22c2d53d702af4587f0a23bf39b642
lukechan2017 <14215837@life.hkbu.edu.hk>
2017-07-10 22:05:03 +0200

    update

--- src/Umbria/TelegramBotBundle/UpdateReceiver/UpdateReceiver.php
+++ src/Umbria/TelegramBotBundle/UpdateReceiver/UpdateReceiver.php
@@ -76,7 +76,8 @@ class UpdateReceiver implements UpdateReceiverInterface
                     break;
                 case "/travelagency":
                     //$arrayOfMessages = $this->executeTravelAgencyQuery(43.105275, 12.391995, 100, true);
-                    $text = "Hello " . $message['from']['first_name'] . ". Today, my suggestion is: ". executeTravelAgencyQuery(43.105275, 12.391995, 100, true);
+                    $a= executeTravelAgencyQuery(43.105275, 12.391995, 100, true);
+                    $text = "Hello " . $message['from']['first_name'] . ". Today, my suggestion is: ";
                     break;
                 case "/help":
                 case "/start":
