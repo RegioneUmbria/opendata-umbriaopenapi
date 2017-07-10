d404cadd590dde5c2ddbc66fd5da6fbaa30f6281
lukechan2017 <14215837@life.hkbu.edu.hk>
2017-07-10 09:39:47 +0200

    update set/hook.php

--- web/hook.php
+++ web/hook.php
@@ -7,6 +7,7 @@ $API_KEY = '401516015:AAGhqAbK_Ni0NZvqwa10b-2aoqiJq630lpw';
 $BOT_NAME = 'lcitbot';
 try {
     // Handle telegram webhook request
+    $telegram = new Shaygan\TelegramBotApiBundle\TelegramBotApi($API_KEY, $BOT_NAME);
     $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     // Silence is golden!
--- web/set.php
+++ web/set.php
@@ -6,10 +6,11 @@ require __DIR__ . "/../vendor/autoload.php";
 $API_KEY = '401516015:AAGhqAbK_Ni0NZvqwa10b-2aoqiJq630lpw';
 $BOT_NAME = 'lcitbot';
 $hook_url = 'https://luke.it/hook.php';
-$certificate_path = "/var/www/html/opendata-umbriaopenapi_luke/lcitbotpp.pem";
+$certificate_path = "/../lcitbotpp.pem";
 
 try {
     // Set webhook
+    $telegram = new Shaygan\TelegramBotApiBundle\TelegramBotApi($API_KEY, $BOT_NAME);
     $result = $telegram->setWebhook($hook_url, ['certificate' => $certificate_path]);
     if ($result->isOk()) {
         echo $result->getDescription();
