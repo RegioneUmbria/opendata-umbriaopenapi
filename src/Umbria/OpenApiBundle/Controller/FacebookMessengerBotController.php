c3f4363b95fdfdf536bf6546bf9f0765e7b14c5d
Lorenzo Ranucci <loryzizu@gmail.com>
2017-07-11 11:31:33 +0200

    Add facebook bot response

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -18,15 +18,47 @@ class FacebookMessengerBotController extends BaseController
 {
     public function indexAction()
     {
+        $response = new Response();
         $challenge = $_REQUEST['hub_challenge'];
         $verify_token = $_REQUEST['hub_verify_token'];
-        // Set this Verify Token Value on your Facebook App
-        $response = new Response();
-        $response->headers->set('Content-Type', 'text/plain');
-        $response->sendHeaders();
-        $response->setContent($challenge);
 
-        return  $response;
+        if ($verify_token === 'testtoken') {
+            // Set this Verify Token Value on your Facebook App
+
+            $response->headers->set('Content-Type', 'text/plain');
+            $response->sendHeaders();
+            $response->setContent($challenge);
+            return $response;
+        }
+
+        $input = json_decode(file_get_contents('php://input'), true);
+// Get the Senders Graph ID
+        $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+// Get the returned message
+        $message = $input['entry'][0]['messaging'][0]['message']['text'];
+//API Url and Access Token, generate this token value on your Facebook App Page
+        $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADeS6lnyqoBALR7gyuSYGk5dYdkzj7r8wLFVS1AxLoAPZCg4NJG2KWOzjs8CIMp2VLApWDbPZC44cnnl0gz1e93oNeEKmZAD2qEk7khJlzVZCMGzVeVZAUOpCN5BXFInZBjJceXebMtRxqzbMjBFJddgEPNczS44qZBSH1urRurQZDZD';
+//Initiate cURL.
+        $ch = curl_init($url);
+//The JSON data.
+        $jsonData = '{
+"recipient":{
+"id":"' . $sender . '"
+},
+"message":{
+"text":"The message you want to return"
+}
+}';
+//Tell cURL that we want to send a POST request.
+        curl_setopt($ch, CURLOPT_POST, 1);
+//Attach our encoded JSON string to the POST fields.
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+//Set the content type to application/json
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+//Execute the request but first check if the message is not empty.
+        if(!empty($input['entry'][0]['messaging'][0]['message'])){
+            $result = curl_exec($ch);
+        }
 
 
     }
