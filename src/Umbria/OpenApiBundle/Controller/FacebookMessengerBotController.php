bf1a69ccd9ac473ae120852ced941a953c93fdd1
lukechan2017 <14215837@life.hkbu.edu.hk>
2017-07-13 11:54:51 +0200

    Fb controller extend?

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -59,7 +59,7 @@ class FacebookMessengerBotController extends BaseController
                     $text .= "help - Visualizzazione comandi disponibili\n";
             }
         }
-        $payload = array("recipient" => array("id" => $sender), "message" => array("text" => $input."\n".$text));
+        $payload = array("recipient" => array("id" => $sender), "message" => array("text" => $sender."\n".$text));
         //Tell cURL that we want to send a POST request.
         curl_setopt($ch, CURLOPT_POST, 1);
         //Attach our encoded JSON string to the POST fields.
