5e1cc87b24b941d60ba69f0956fe7a0357dda407
liaonanqiang <16417844@life.hkbu.edu.hk>
2017-07-13 11:15:22 +0200

    updatereceiver

--- src/Umbria/TelegramBotBundle/UpdateReceiver/UpdateReceiver.php
+++ src/Umbria/TelegramBotBundle/UpdateReceiver/UpdateReceiver.php
@@ -1,52 +1,42 @@
 <?php
-
 namespace Umbria\TelegramBotBundle\UpdateReceiver;
-
 use AnthonyMartin\GeoLocation\GeoLocation;
 use JMS\DiExtraBundle\Annotation as DI;
 use Shaygan\TelegramBotApiBundle\TelegramBotApi;
 use Shaygan\TelegramBotApiBundle\Type\Update;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use TelegramBot\Api\Types\ReplyKeyboardMarkup;
-use Umbria\OpenApiBundle\Entity\Tourism\GraphsEntities\SportFacility;
 use Umbria\OpenApiBundle\Repository\Tourism\GraphsEntities\AttractorRepository;
 use Umbria\OpenApiBundle\Repository\Tourism\GraphsEntities\ProposalRepository;
-
 class UpdateReceiver implements UpdateReceiverInterface
 {
     private $telegramBotApi;
     private $config;
-
     /**
      * @var \Doctrine\ORM\EntityManager
      */
     private $em;
-
     public function __construct(TelegramBotApi $telegramBotApi, $config, $em)
     {
         $this->telegramBotApi = $telegramBotApi;
         $this->config = $config;
         $this->em = $em;
     }
-
     public function handleUpdate(Update $update)
     {
         $arrayOfArraysOfStrings = array(
-            array("/about", "/hello","/help","/sport")
+            array("/about", "/hello","/help")
         );
         $newKeyboard = new ReplyKeyboardMarkup($arrayOfArraysOfStrings, true, true);
         $message = json_decode(json_encode($update->message), true);
-
-        // LOCATION
+// LOCATION
         if (isset($message['location'])) {
             $latitude =$message['location']['latitude'];
             $longitude = $message['location']['longitude'];
-
-            // Controllo se all'interno dell'Umbria
+// Controllo se all'interno dell'Umbria
             if (($latitude >= 42.36 AND $latitude <= 43.60)
                 AND ($longitude >= 11.88 AND $longitude <= 13.25)
             ) {
-
                 $this->telegramBotApi->sendMessage($message['chat']['id'], "Vicino a te puoi trovare questa proposta di visita:");
                 $arrayOfMessages = $this->executeProposalQuery($latitude, $longitude, 10);
                 for ($i = 0; $i < count($arrayOfMessages); $i++) {
@@ -63,9 +53,7 @@ class UpdateReceiver implements UpdateReceiverInterface
                 $text = "Ciao " . $message['from']['first_name'] . ". Sei troppo lontano dall'Umbria. Da noi puoi trovare: " . $arrayOfMessages[0];
                 $this->telegramBotApi->sendMessage($message['chat']['id'], $text);
             }
-
         } else if (isset($message['text'])) {
-
             switch ($message['text']) {
                 case "/about":
                     $text = "UmbriaTourismBot ti permette di ricevere informazioni turistiche. Invia la tua posizione per scoprire tutte le bellezze che la nostra regione ha in serbo per te";
@@ -74,11 +62,6 @@ class UpdateReceiver implements UpdateReceiverInterface
                     $arrayOfMessages = $this->executeAttractorQuery(43.105275, 12.391995, 100, true);
                     $text = "Ciao " . $message['from']['first_name'] . ". Oggi ti consiglio: " . $arrayOfMessages[0];
                     break;
-                case "/sport":
-                    $arrayOfMessages1 = $this->executeSportFacilityQuery(43.105275, 12.391995, 100, true);
-                    $text = "Ciao " . $message['from']['first_name'] . ". Oggi ti consiglio: " . $arrayOfMessages1[0];
-                    break;
-
                 case "/help":
                 case "/start":
                     $text = "UmbriaTourismBot ti permette di ricevere informazioni turistiche. Invia la tua posizione per scoprire tutte le bellezze che la nostra regione ha in serbo per te\n\n";
@@ -87,38 +70,30 @@ class UpdateReceiver implements UpdateReceiverInterface
                     $text .= "/about - Informazioni sul bot\n";
                     $text .= "/hello - Suggerimenti\n";
                     $text .= "/help - Visualizzazione comandi disponibili\n";
-                    $text .= "/sprot -Trova impianti sportivi\n";
                     break;
             }
-
             $newKeyboardCond = $message['text'];
             if (strcmp($newKeyboardCond, "/start") XOR strcmp($newKeyboardCond, "/help")) {
                 $this->telegramBotApi->sendMessage($message['chat']['id'], $text, null, false, null, $newKeyboard);
             } else $this->telegramBotApi->sendMessage($message['chat']['id'], $text);
         }
-
     }
-
     public function executeAttractorQuery($lat, $lng, $radius, $rand)
     {
         /**@var AttractorRepository $attractorRepo */
         $attractorRepo = $this->em->getRepository('UmbriaOpenApiBundle:Tourism\GraphsEntities\Attractor');
-
         $location = GeoLocation::fromDegrees($lat, $lng);
         /** @var GeoLocation[] $bounds */
         /** @noinspection PhpInternalEntityUsedInspection */
         $bounds = $location->boundingCoordinates($radius, 'km');
-
         $pois = $attractorRepo->findByPosition(
             $bounds[1]->getLatitudeInDegrees(),
             $bounds[0]->getLatitudeInDegrees(),
             $bounds[1]->getLongitudeInDegrees(),
             $bounds[0]->getLongitudeInDegrees());
-
         if (sizeof($pois) > 0) {
             if ($rand) {
                 $key = array_rand($pois);
-
                 $poi = $pois[$key];
                 $stringResult[0] = $poi->getName() . "\n" . str_replace('&nbsp;', ' ', strip_tags($poi->getShortDescription())) . "\n" . $poi->getResourceOriginUrl();
                 return $stringResult;
@@ -138,27 +113,22 @@ class UpdateReceiver implements UpdateReceiverInterface
     {
         /**@var ProposalRepository $proposalRepo */
         $proposalRepo = $this->em->getRepository('UmbriaOpenApiBundle:Tourism\GraphsEntities\Proposal');
-
         $location = GeoLocation::fromDegrees($lat, $lng);
         /** @var GeoLocation[] $bounds */
         /** @noinspection PhpInternalEntityUsedInspection */
         $bounds = $location->boundingCoordinates($radius, 'km');
-
         $pois = $proposalRepo->findByPosition(
             $bounds[1]->getLatitudeInDegrees(),
             $bounds[0]->getLatitudeInDegrees(),
             $bounds[1]->getLongitudeInDegrees(),
             $bounds[0]->getLongitudeInDegrees());
-
         if (sizeof($pois) > 0) {
             $key = array_rand($pois);
             $poi = $pois[$key];
             $stringResult[0] = $poi->getName() . "\n" . str_replace('&nbsp;', ' ', strip_tags($poi->getShortDescription())) . "\n" . $poi->getResourceOriginUrl();
             return $stringResult;
-
         } else {
             throw new Exception();
         }
     }
-
 }
