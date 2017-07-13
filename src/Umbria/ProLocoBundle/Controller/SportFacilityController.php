0319bce0c428797fcf143f3f91b93852cfafefc1
liaonanqiang <16417844@life.hkbu.edu.hk>
2017-07-13 15:37:47 +0200

    test

--- src/Umbria/ProLocoBundle/Controller/SportFacilityController.php
+++ src/Umbria/ProLocoBundle/Controller/SportFacilityController.php
@@ -25,7 +25,46 @@ class SportFacilityController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $itemsOnPage = $this->container->getParameter('items_on_page');
 
+        $searchFilter = new SearchFilter();
+
+        $form = $this->createFormBuilder($searchFilter)
+            ->add("text", TextType::class, array('required' => false))
+            ->add('search', SubmitType::class, array('label' => 'Cerca'))
+            ->getForm();
+
+        $form->handleRequest($request);
+        $text = "";
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $searchFilter = $form->getData();
+            $text = $searchFilter->getText();
+            $form = $this->createFormBuilder($searchFilter)
+                ->add("text", TextType::class, array('required' => false))
+                ->add('search', SubmitType::class, array('label' => 'Ricerca'))
+                ->getForm();
+        }
+
+        $repository = $this->getDoctrine()
+            ->getRepository('UmbriaOpenApiBundle:Tourism\GraphsEntities\SportFacility');
+        $qb = $repository->createQueryBuilder('a');
+        $query = $qb
+            ->where($qb->expr()->like('a.name', '?1'))
+            ->setParameter(1, '%' . $text . '%');
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            $itemsOnPage/*limit per page*/
+        );
+
+        return $this->render('UmbriaProLocoBundle:SportFacility:index.html.twig', array(
+            'pagination' => $pagination,
+            'form' => $form->createView(),
+        ));
     }
 
     /**
@@ -37,5 +76,11 @@ class SportFacilityController extends Controller
      */
     public function showAction($id)
     {
+        $repository = $this->getDoctrine()
+            ->getRepository('UmbriaOpenApiBundle:Tourism\GraphsEntities\SportFacility');
+        $sportFacility = $repository->findById($id);
+        return $this->render('UmbriaProLocoBundle:SportFacility:show.html.twig', array(
+            'sportFacility' => $sportFacility[0]
+        ));
     }
 }
