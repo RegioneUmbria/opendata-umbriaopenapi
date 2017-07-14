99e5209c200d78805072d113d67e00b8677baa75
liaonanqiang <16417844@life.hkbu.edu.hk>
2017-07-14 10:29:31 +0200

    test1

---
+++ app/cache/prod/jms_diextra/metadata/Umbria-OpenApiBundle-Repository-Tourism-GraphsEntities-EventRepository.cache.php
@@ -0,0 +1,1 @@
+<?php return unserialize('C:21:"Metadata\\NullMetadata":135:{a:5:{i:0;s:70:"Umbria\\OpenApiBundle\\Repository\\Tourism\\GraphsEntities\\EventRepository";i:1;a:0:{}i:2;a:0:{}i:3;a:0:{}i:4;i:1500020894;}}');
---
+++ app/cache/prod/jms_diextra/metadata/Umbria-OpenApiBundle-Repository-Tourism-GraphsEntities-SportFacilityRepository.cache.php
@@ -0,0 +1,1 @@
+<?php return unserialize('C:21:"Metadata\\NullMetadata":143:{a:5:{i:0;s:78:"Umbria\\OpenApiBundle\\Repository\\Tourism\\GraphsEntities\\SportFacilityRepository";i:1;a:0:{}i:2;a:0:{}i:3;a:0:{}i:4;i:1500020894;}}');
