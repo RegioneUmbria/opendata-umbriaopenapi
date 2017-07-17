2ebb3aaef35ed11d0c636fe2ebbdeba67f5fdc30
liaonanqiang <16417844@life.hkbu.edu.hk>
2017-07-17 16:03:33 +0200

    facebookbot

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -12,6 +12,7 @@ class FacebookMessengerBotController extends BaseController
 {
     public function indexAction()
     {
+
         $response = new Response();
         $challenge = $_REQUEST['hub_challenge'];
         $verify_token = $_REQUEST['hub_verify_token'];
@@ -27,24 +28,44 @@ class FacebookMessengerBotController extends BaseController
         $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 // Get the returned message
         $message = $input['entry'][0]['messaging'][0]['message']['text'];
+
+        $response = null;
+        if($message == "hi") {
+            $answer = "Hello";
+        }
+        $response = [
+            'recipient' => [ 'id' => $sender ],
+            'message' => [ 'text' => $answer ]
+        ];
+
 //API Url and Access Token, generate this token value on your Facebook App Page
-        $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADeS6lnyqoBALR7gyuSYGk5dYdkzj7r8wLFVS1AxLoAPZCg4NJG2KWOzjs8CIMp2VLApWDbPZC44cnnl0gz1e93oNeEKmZAD2qEk7khJlzVZCMGzVeVZAUOpCN5BXFInZBjJceXebMtRxqzbMjBFJddgEPNczS44qZBSH1urRurQZDZD';
+        //$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADeS6lnyqoBALR7gyuSYGk5dYdkzj7r8wLFVS1AxLoAPZCg4NJG2KWOzjs8CIMp2VLApWDbPZC44cnnl0gz1e93oNeEKmZAD2qEk7khJlzVZCMGzVeVZAUOpCN5BXFInZBjJceXebMtRxqzbMjBFJddgEPNczS44qZBSH1urRurQZDZD';
+        $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAEWX2RC5XoBABm2kTFBeACAB8LfaWC7UHbTU273UobfN1vNzoj8qj1idjIjHC0LJytwfzZCC4ZCJ8OqQcKOmN3K3xr4o1bwYmWYWZA0BCV7B2ZCUgJmse7M1SwwE7sCVz0MKpv2YA3U052rLuZCbwbbVBD8y90rzntENprWDUQZDZD';
+
 //Initiate cURL.
         $ch = curl_init($url);
-        $payload = array("recipient" => array("id" => $sender), "message" => array("text" => "Benvenuto su UmbriaOpenAPI"));
+       // $payload = array("recipient" => array("id" => $sender), "message" => array("text" => "Benvenuto su UmbriaOpenAPI"));
 //Tell cURL that we want to send a POST request.
         curl_setopt($ch, CURLOPT_POST, 1);
 //Attach our encoded JSON string to the POST fields.
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+        //curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 //Set the content type to application/json
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 //Execute the request but first check if the message is not empty.
-        if (!empty($input['entry'][0]['messaging'][0]['message'])) {
+//        if (!empty($input['entry'][0]['messaging'][0]['message'])) {
+//            $result = curl_exec($ch);
+//        }
+//        $logger = $this->get('logger');
+//        $logger->info(json_encode($payload));
+//        $response->setContent(json_encode($payload));
+//        return $response;
+
+        if(!empty($input)){
             $result = curl_exec($ch);
         }
-        $logger = $this->get('logger');
-        $logger->info(json_encode($payload));
-        $response->setContent(json_encode($payload));
-        return $response;
+        curl_close($ch);
+
     }
+
 }
