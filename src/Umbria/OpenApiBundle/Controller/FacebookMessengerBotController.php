f2d966097821755a052721a07d412669d388e0cc
lukechan2017 <14215837@life.hkbu.edu.hk>
2017-07-17 11:29:43 +0200

    extend fbcontroller

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -47,7 +47,16 @@ class FacebookMessengerBotController extends BaseController
                     break;
                 case "hello":
                     //$arrayOfMessages = $this->executeAttractorQuery(43.105275, 12.391995, 100, true);
-                    $text = "Ciao, Oggi ti consiglio: " . $arrayOfMessages[0];
+                    $text = "Ciao, Oggi ti consiglio: ";
+                    $sql="SELECT name,shortDescription,resourceOriginUrl FROM tourism_attractor";
+                    $result = mysqli_query($sql);
+                    while ($row =mysqli_fetch_array($result)){
+                        $aname=$row['name'];
+                        $ashortDescription=$row['shortDescription'];
+                        $aresourceOriginUrl=$row['resourceOriginUrl'];
+                        $text=$text."/n".$aname."/n".$ashortDescription."/n".$aresourceOriginUrl;
+                    }
+
                     break;
                 case "event":
                    // $arrayOfMessages = $this->executeEventQuery(43.105275, 12.391995, 100, true);
