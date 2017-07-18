bba6f465e432ac6fcef7e8df596869827f2d29d0
liaonanqiang <16417844@life.hkbu.edu.hk>
2017-07-18 15:58:46 +0200

    facebook0

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -16,90 +16,40 @@ class FacebookMessengerBotController extends BaseController
     public function indexAction()
     {
 
-        $response = new Response();
-        $challenge = $_REQUEST['hub_challenge'];
-        $verify_token = $_REQUEST['hub_verify_token'];
-        if ($verify_token === 'testtoken') {
-// Set this Verify Token Value on your Facebook App
-            $response->headers->set('Content-Type', 'text/plain');
-            $response->sendHeaders();
-            $response->setContent($challenge);
-            return $response;
+
+        $hubVerifyToken = 'testtoken';
+        $accessToken = "EAAEWX2RC5XoBABm2kTFBeACAB8LfaWC7UHbTU273UobfN1vNzoj8qj1idjIjHC0LJytwfzZCC4ZCJ8OqQcKOmN3K3xr4o1bwYmWYWZA0BCV7B2ZCUgJmse7M1SwwE7sCVz0MKpv2YA3U052rLuZCbwbbVBD8y90rzntENprWDUQZDZD";
+// check token at setup
+        if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+            echo $_REQUEST['hub_challenge'];
+            exit;
         }
+// handle bot's anwser
         $input = json_decode(file_get_contents('php://input'), true);
-// Get the Senders Graph ID
         $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-// Get the returned message
         $message = $input['entry'][0]['messaging'][0]['message']['text'];
-        /**
-         * Some Basic rules to validate incoming messages
-         */
-        if(preg_match('[time|current time|now]', strtolower($message))) {
-
-            // Make request to Time API
-            ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
-            $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
-            if($result == 'more') {
-//                $message_to_reply = $result;
-                $message_to_reply = ["attachment" => [
-                    "type" => "template",
-                    "payload" => [
-                        "template_type" => "button",
-                        "text" => "What do you want to do next?",
-                        "buttons" => [
-                            [
-                                "type" => "web_url",
-                                "url" => "https://umbriaopenapi-nigel.tk/",
-                                "title" => "Show Website"
-                            ],
-                            [
-                                "type" => "postback",
-                                "title" => "Start Chatting",
-                                "payload" => "USER_DEFINED_PAYLOAD"
-                            ]
-                        ]
-                    ]
-                ]];
-            }
-        } else {
-            $message_to_reply = 'Huh! what do you mean?';
+        $response = null;
+//set Message
+        if ($message == "hi") {
+            $answer = "Hello";
         }
-        print $message_to_reply;
-
-//API Url and Access Token, generate this token value on your Facebook App Page
+//send message to facebook bot
+        $response = [
+            'recipient' => ['id' => $sender],
+            'message' => ['text' => $answer]
+        ];
         $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAEWX2RC5XoBABm2kTFBeACAB8LfaWC7UHbTU273UobfN1vNzoj8qj1idjIjHC0LJytwfzZCC4ZCJ8OqQcKOmN3K3xr4o1bwYmWYWZA0BCV7B2ZCUgJmse7M1SwwE7sCVz0MKpv2YA3U052rLuZCbwbbVBD8y90rzntENprWDUQZDZD';
 
 //Initiate cURL.
         $ch = curl_init($url);
-
-        //The JSON data.
-        $jsonData = '{
-    "recipient":{
-        "id":"'.$sender.'"
-    },
-    "message":{
-        "text":"'.$message_to_reply.'"
-    }
-}';
-
-//Encode the array into JSON.
-        $jsonDataEncoded = $jsonData;
-
-//Tell cURL that we want to send a POST request.
+//        $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . $accessToken);
         curl_setopt($ch, CURLOPT_POST, 1);
-
-//Attach our encoded JSON string to the POST fields.
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-
-//Set the content type to application/json
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-
-//Execute the request
-        if(!empty($input['entry'][0]['messaging'][0]['message'])){
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        if (!empty($input)) {
             $result = curl_exec($ch);
         }
-
+        curl_close($ch);
     }
 
 }
