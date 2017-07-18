ba1767ee5697bbd65e61a40d0e5efdbe2f11c75d
liaonanqiang <16417844@life.hkbu.edu.hk>
2017-07-18 11:07:52 +0200

    facebookf

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -38,31 +38,34 @@ class FacebookMessengerBotController extends BaseController
             ];
             return $response;
         }
-        if($message == "more") {
+
+        else if($message == "website"){
             $answer = ["attachment"=>[
                 "type"=>"template",
                 "payload"=>[
-                    "template_type"=>"button",
-                    "text"=>"What do you want to do next?",
-                    "buttons"=>[
+                    "template_type"=>"generic",
+                    "elements"=>[
                         [
-                            "type"=>"web_url",
-                            "url"=>"https://umbriaopenapi-nigel.tk/",
-                            "title"=>"Show Website"
-                        ],
-                        [
-                            "type"=>"postback",
-                            "title"=>"Start Chatting",
-                            "payload"=>"USER_DEFINED_PAYLOAD"
+                            "title"=>"Welcome to UmbriaDigitale\'s Website",
+                            "item_url"=>"https://umbriaopenapi-nigel.tk/",
+                            "image_url"=>"https://goo.gl/images/PLZ9Mg",
+                            "subtitle"=>"We\'ve got the right hat for everyone.",
+                            "buttons"=>[
+                                [
+                                    "type"=>"web_url",
+                                    "url"=>"https://umbriaopenapi-nigel.tk/",
+                                    "title"=>"View Website"
+                                ]
+                            ]
                         ]
                     ]
                 ]
             ]];
             $response = [
-                'recipient' => [ 'id' => $sender],
+                'recipient' => [ 'id' => $sender ],
                 'message' => $answer
             ];
-        }
+            return $response;}
 //API Url and Access Token, generate this token value on your Facebook App Page
         //$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADeS6lnyqoBALR7gyuSYGk5dYdkzj7r8wLFVS1AxLoAPZCg4NJG2KWOzjs8CIMp2VLApWDbPZC44cnnl0gz1e93oNeEKmZAD2qEk7khJlzVZCMGzVeVZAUOpCN5BXFInZBjJceXebMtRxqzbMjBFJddgEPNczS44qZBSH1urRurQZDZD';
         $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAEWX2RC5XoBABm2kTFBeACAB8LfaWC7UHbTU273UobfN1vNzoj8qj1idjIjHC0LJytwfzZCC4ZCJ8OqQcKOmN3K3xr4o1bwYmWYWZA0BCV7B2ZCUgJmse7M1SwwE7sCVz0MKpv2YA3U052rLuZCbwbbVBD8y90rzntENprWDUQZDZD';
