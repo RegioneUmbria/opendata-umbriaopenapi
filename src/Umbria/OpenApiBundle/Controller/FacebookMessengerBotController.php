8d76eebd2f649254940438438b61ad36280d1d16
liaonanqiang <16417844@life.hkbu.edu.hk>
2017-07-18 15:12:47 +0200

    facebook0

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -122,8 +122,8 @@ class FacebookMessengerBotController extends BaseController
 //Tell cURL that we want to send a POST request.
         curl_setopt($ch, CURLOPT_POST, 1);
 //Attach our encoded JSON string to the POST fields.
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($jsonDataEncoded));
-//        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($jsonDataEncoded));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
 //Set the content type to application/json
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 //Execute the request but first check if the message is not empty.
