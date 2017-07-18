5b62ed8bf749084324d03bd49d42ded2cd51aa01
lukechan2017 <14215837@life.hkbu.edu.hk>
2017-07-18 09:36:43 +0200

    Using ORM

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -58,7 +58,8 @@ class FacebookMessengerBotController extends BaseController
                 case "hello":
                 case "Hello":
                     $arrayOfMessages = $this->executeAttractorQuery(43.105275, 12.391995, 100, true);
-                    $text ="Ciao " .". Oggi ti consiglio: " . $arrayOfMessages[0];
+                    $ans =executeAttractorQuery(43.105275, 12.391995, 100, true);
+                    $text ="Ciao " .". Oggi ti consiglio: " . $ans;
 
 //                    $sql="SELECT name,shortDescription,resourceOriginUrl FROM tourism_attractor ORDER BY RAND()LIMIT 1";
 //                    $result = mysqli_query($conn,$sql);
