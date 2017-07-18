3c202bf3d9b63540b59b618154c61d177dbe28e6
liaonanqiang <16417844@life.hkbu.edu.hk>
2017-07-18 11:24:27 +0200

    facebookc

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -30,13 +30,38 @@ class FacebookMessengerBotController extends BaseController
         $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
         $response = null;
-        if($message == "hi") {
+        if ($message == "hi") {
             $answer = "Hello";
-        }
         $response = [
-            'recipient' => [ 'id' => $sender ],
-            'message' => [ 'text' => $answer ]
+            'recipient' => ['id' => $sender],
+            'message' => ['text' => $answer]
         ];
+    }
+    else if($message == "more") {
+        $answer = ["attachment"=>[
+            "type"=>"template",
+            "payload"=>[
+                "template_type"=>"button",
+                "text"=>"What do you want to do next?",
+                "buttons"=>[
+                    [
+                        "type"=>"web_url",
+                        "url"=>"https://umbriaopenapi-nigel.tk/",
+                        "title"=>"Show Website"
+                    ],
+                    [
+                        "type"=>"postback",
+                        "title"=>"Start Chatting",
+                        "payload"=>"USER_DEFINED_PAYLOAD"
+                    ]
+                ]
+            ]
+        ]];
+        $response = [
+            'recipient' => [ 'id' => $sender],
+            'message' => $answer
+        ];
+    }
 
 //API Url and Access Token, generate this token value on your Facebook App Page
         //$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADeS6lnyqoBALR7gyuSYGk5dYdkzj7r8wLFVS1AxLoAPZCg4NJG2KWOzjs8CIMp2VLApWDbPZC44cnnl0gz1e93oNeEKmZAD2qEk7khJlzVZCMGzVeVZAUOpCN5BXFInZBjJceXebMtRxqzbMjBFJddgEPNczS44qZBSH1urRurQZDZD';
