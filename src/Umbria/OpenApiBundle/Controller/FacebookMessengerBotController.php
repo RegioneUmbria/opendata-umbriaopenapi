c2c0eab1e96ff37bc5a263a5bcb1ab0c08c61198
liaonanqiang <16417844@life.hkbu.edu.hk>
2017-07-19 09:52:26 +0200

    face1

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -5,55 +5,51 @@
  * Date: 11/07/2017
  * Time: 10:30
  */
-
 namespace Umbria\OpenApiBundle\Controller;
-
 use Symfony\Component\HttpFoundation\Response;
 use Umbria\OpenApiBundle\Controller\Tourism\BaseController;
-
 class FacebookMessengerBotController extends BaseController
 {
     public function indexAction()
     {
-
+        $response = new Response();
         $answer = null;
-        $hubVerifyToken = 'testtoken';
-        $accessToken = "EAAEWX2RC5XoBABm2kTFBeACAB8LfaWC7UHbTU273UobfN1vNzoj8qj1idjIjHC0LJytwfzZCC4ZCJ8OqQcKOmN3K3xr4o1bwYmWYWZA0BCV7B2ZCUgJmse7M1SwwE7sCVz0MKpv2YA3U052rLuZCbwbbVBD8y90rzntENprWDUQZDZD";
-// check token at setup
-        if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
-            echo $_REQUEST['hub_challenge'];
-            exit;
+        $challenge = $_REQUEST['hub_challenge'];
+        $verify_token = $_REQUEST['hub_verify_token'];
+        if ($verify_token === 'testtoken') {
+            // Set this Verify Token Value on your Facebook App
+            $response->headers->set('Content-Type', 'text/plain');
+            $response->sendHeaders();
+            $response->setContent($challenge);
+            return $response;
         }
-// handle bot's anwser
         $input = json_decode(file_get_contents('php://input'), true);
+        // Get the Senders Graph ID
         $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+        // Get the returned message
         $message = $input['entry'][0]['messaging'][0]['message']['text'];
-        $response = null;
-//set Message
+        //set Message
         if ($message == "hi") {
             $answer = "Hello";
         }
-//send message to facebook bot
-        $response = [
-            'recipient' => ['id' => $sender],
-            'message' => ['text' => $answer]
-        ];
+        //API Url and Access Token, generate this token value on your Facebook App Page
         $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAEWX2RC5XoBABm2kTFBeACAB8LfaWC7UHbTU273UobfN1vNzoj8qj1idjIjHC0LJytwfzZCC4ZCJ8OqQcKOmN3K3xr4o1bwYmWYWZA0BCV7B2ZCUgJmse7M1SwwE7sCVz0MKpv2YA3U052rLuZCbwbbVBD8y90rzntENprWDUQZDZD';
-
-//Initiate cURL.
+        //Initiate cURL.
         $ch = curl_init($url);
-//        $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . $accessToken);
+        $payload = array("recipient" => array("id" => $sender), "message" => array("text" => $answer));
+        //Tell cURL that we want to send a POST request.
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-        if (!empty($input)) {
+        //Attach our encoded JSON string to the POST fields.
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+        //Set the content type to application/json
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        //Execute the request but first check if the message is not empty.
+        if (!empty($input['entry'][0]['messaging'][0]['message'])) {
             $result = curl_exec($ch);
         }
-        curl_close($ch);
         $logger = $this->get('logger');
-        $logger->info(json_encode($response));
-        $response->setContent(json_encode($response));
+        $logger->info(json_encode($payload));
+        $response->setContent(json_encode($payload));
         return $response;
     }
-
 }
