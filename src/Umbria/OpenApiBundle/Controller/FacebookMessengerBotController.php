fd332c575f55d9e39bcd4091e90d465779756f8f
lukechan2017 <14215837@life.hkbu.edu.hk>
2017-07-20 15:16:49 +0200

    Fixing ...

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -43,20 +43,270 @@ class FacebookMessengerBotController extends BaseController
         //$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAALdAertaysBAE4TLVKCiIsZBeJMhOl4dq0i7XT9QIy7Xfir87JIjC5ldDNulS8ZAhilEzoq7JZAmGTK1e3ZCPG9Lyv5EmJnrIekeJJD3eZB37M2TUaSoZAH53wdOuCJTdF2m6QZA5ZBoZB6RNPdXll5M6DGCEZAzqhwZCvVZCWRlAhtWK0xWd3ApwMXr5qf0ALo90UZD';
         //Initiate cURL.
         $ch = curl_init($url);
-        $payload = array("recipient" => array("id" => $sender), "message" => array("text" => "Benvenuto su UmbriaOpenAPI"));
-        //Tell cURL that we want to send a POST request.
-        curl_setopt($ch, CURLOPT_POST, 1);
-        //Attach our encoded JSON string to the POST fields.
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
-        //Set the content type to application/json
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        //Execute the request but first check if the message is not empty.
-        if (!empty($input['entry'][0]['messaging'][0]['message'])) {
-            $result = curl_exec($ch);
+
+        // --------------------------------------------@20170718--------------------------------------------
+        $sendermessage = $message;
+        if ($message) {
+            $imageurl = "@";
+            $content = "@";
+            $text = "Welcome to UmbiraOpenApi";
+            if (isset($sendermessage)) {
+                switch ($sendermessage) {
+                    case "about":
+                    case "About":
+                        $text = "UmbriaTourismBot ti permette di ricevere informazioni turistiche. Invia la tua posizione per scoprire tutte le bellezze che la nostra regione ha in serbo per te";
+                        break;
+                    case "hello":
+                    case "Hello":
+                        $arrayOfMessages = $this->executeAttractorQuery(43.105275, 12.391995, 100, true);
+                        $title= $arrayOfMessages[0];
+                        $imageurl=$arrayOfMessages[1];
+                        $subtitle=$arrayOfMessages[2];
+                        $ResourceOriginUrl=$arrayOfMessages[3];
+                        $text = "Ciao, Oggi ti consiglio: " . "\n".$title;
+                        $content = "Descrizione : \n".$subtitle."\n".$ResourceOriginUrl;
+                        break;
+                    case "event":
+                    case "Event":
+                        $arrayOfMessages = $this->executeEventQuery(43.105275, 12.391995, 100, true);
+                        $title= $arrayOfMessages[0];
+                        $imageurl=$arrayOfMessages[1];
+                        $subtitle=$arrayOfMessages[2];
+                        $ResourceOriginUrl=$arrayOfMessages[3];
+                        $startDate=$arrayOfMessages[4];
+                        $endDate=$arrayOfMessages[5];
+                        $text = "Ciao, Oggi ti consiglio: " . "\n".$title;
+                        $content = "Durata : dal ".$startDate." al ".$endDate."\n"."Descrizione : ";//.$subtitle.
+                        $content = $content."\n".$ResourceOriginUrl;
+                        break;
+                    case "travelagency":
+                    case "Travelagency":
+                        $arrayOfMessages = $this->executeTravelAgencyQuery(43.105275, 12.391995, 100, true);
+                        $title= $arrayOfMessages[0];
+                        $imageurl=$arrayOfMessages[1];
+                        $subtitle=$arrayOfMessages[2];
+                        $ResourceOriginUrl=$arrayOfMessages[3];
+                        $telephone=$arrayOfMessages[4];
+                        $fax=$arrayOfMessages[5];
+                        $email=$arrayOfMessages[6];
+                        $text = "Ciao, Oggi ti consiglio: " . "\n".$title;
+                        $content = "Descrizione : \n";
+                        if (!is_null($telephone)){
+                            $content=$content."\tTelephone : ".$telephone;
+                        }
+                        if (!is_null($fax)){
+                            $content=$content."\n\tFax : ".$fax;
+                        }
+                        if (!is_null($email)){
+                            $content=$content."\n\temail : ".$email;
+                        }
+                        $content =$content."\n".$ResourceOriginUrl;
+                        break;
+                    case "help":
+                    case "Help":
+                    case "start":
+                    case "Start":
+                        $text = "UmbriaTourismBot ti permette di ricevere informazioni turistiche. Invia la tua posizione per scoprire tutte le bellezze che la nostra regione ha in serbo per te\n\n";
+                        break;
+                    default :
+                        $text = "Lista comandi:\n";
+                        $text .= "About - Informazioni sul bot\n";
+                        $text .= "Event - Informazioni sul eventi\n";
+                        $text .= "Travelagency - Informazioni sul agenzia di viaggi\n";
+                        $text .= "Hello - Suggerimenti\n";
+                        $text .= "Help - Visualizzazione comandi disponibili\n";
+                }
+            }
+            //--------------------------------------------------------------------------------------------------
+                //Sending the title
+            if (strcasecmp($imageurl,"@")!=0) {
+                $payload = array("recipient" => array("id" => $sender), "message" => array( "text"=>$text));
+                //Tell cURL that we want to send a POST request.
+                curl_setopt($ch, CURLOPT_POST, 1);
+                //Attach our encoded JSON string to the POST fields.
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+                //Set the content type to apsplication/json
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+                //Execute the request but first check if the message is not empty.
+                if (!empty($input['entry'][0]['messaging'][0]['message'])) {
+                    $result = curl_exec($ch);
+                }
+                $logger = $this->get('logger');
+                $logger->info(json_encode($payload));
+                $response->setContent(json_encode($payload));
+
+                //Sending the First Imamge
+                $payload = array("recipient" => array("id" => $sender), "message" => array("attachment" => array("type" => "image", "payload" => array("url" => $imageurl))));
+                //Tell cURL that we want to send a POST request.
+                curl_setopt($ch, CURLOPT_POST, 1);
+                //Attach our encoded JSON string to the POST fields.
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+                //Set the content type to apsplication/json
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+                //Execute the request but first check if the message is not empty.
+                if (!empty($input['entry'][0]['messaging'][0]['message'])) {
+                    $result = curl_exec($ch);
+                }
+                $logger = $this->get('logger');
+                $logger->info(json_encode($payload));
+                $response->setContent(json_encode($payload));
+                //Sending the Description and the ResourceOriginUrl
+                $payload = array("recipient" => array("id" => $sender), "message" => array( "text"=>$content));
+            }else {
+                //Sending the Description and the ResourceOriginUrl
+                $payload = array("recipient" => array("id" => $sender), "message" => array( "text"=>$text."\n".$content));
+            }
+
+            //Tell cURL that we want to send a POST request.
+            curl_setopt($ch, CURLOPT_POST, 1);
+            //Attach our encoded JSON string to the POST fields.
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+            //Set the content type to apsplication/json
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            //Execute the request but first check if the message is not empty.
+            if (!empty($input['entry'][0]['messaging'][0]['message'])) {
+                $result = curl_exec($ch);
+            }
+            $logger = $this->get('logger');
+            $logger->info(json_encode($payload));
+            $response->setContent(json_encode($payload));
+
+            return $response;
         }
-        $logger = $this->get('logger');
-        $logger->info(json_encode($payload));
-        $response->setContent(json_encode($payload));
-        return $response;
     }
+
+    public function executeAttractorQuery($lat, $lng, $radius, $rand)
+    {
+        /**@var AttractorRepository $attractorRepo */
+        $attractorRepo = $this->getDoctrine()->getRepository('UmbriaOpenApiBundle:Tourism\GraphsEntities\Attractor');
+
+        $location = GeoLocation::fromDegrees($lat, $lng);
+        /** @var GeoLocation[] $bounds */
+        /** @noinspection PhpInternalEntityUsedInspection */
+        $bounds = $location->boundingCoordinates($radius, 'km');
+
+        $pois = $attractorRepo->findByPosition(
+            $bounds[1]->getLatitudeInDegrees(),
+            $bounds[0]->getLatitudeInDegrees(),
+            $bounds[1]->getLongitudeInDegrees(),
+            $bounds[0]->getLongitudeInDegrees());
+
+        if (sizeof($pois) > 0) {
+            if ($rand) {
+                $key = array_rand($pois);
+                $poi = $pois[$key];
+                //$stringResult[0] = $poi->getName() . "\nDescriptions : " . str_replace('&nbsp;', ' ', strip_tags($poi->getDescriptions())) . "\n" . $poi->getResourceOriginUrl();
+                $stringResult[0] = $poi->getName();
+                $stringResult[1] = $poi->getImages()[0];
+                $stringResult[2] = str_replace('&nbsp;', ' ', strip_tags($poi->getShortDescription())) ;
+                $stringResult[3] = $poi->getResourceOriginUrl();
+                return $stringResult;
+            }
+        } else {
+            throw new Exception();
+        }
+    }
+
+    public function executeProposalQuery($lat, $lng, $radius,$rand)
+    {
+        /**@var ProposalRepository $proposalRepo */
+        $proposalRepo = $this->getDoctrine()->getRepository('UmbriaOpenApiBundle:Tourism\GraphsEntities\Proposal');
+
+        $location = GeoLocation::fromDegrees($lat, $lng);
+        /** @var GeoLocation[] $bounds */
+        /** @noinspection PhpInternalEntityUsedInspection */
+        $bounds = $location->boundingCoordinates($radius, 'km');
+
+        $pois = $proposalRepo->findByPosition(
+            $bounds[1]->getLatitudeInDegrees(),
+            $bounds[0]->getLatitudeInDegrees(),
+            $bounds[1]->getLongitudeInDegrees(),
+            $bounds[0]->getLongitudeInDegrees());
+
+        if (sizeof($pois) > 0) {
+            if ($rand) {
+                $key = array_rand($pois);
+                $poi = $pois[$key];
+                //$stringResult[0] = $poi->getName() . "\nDescriptions : " . str_replace('&nbsp;', ' ', strip_tags($poi->getDescriptions())) . "\n" . $poi->getResourceOriginUrl();
+                $stringResult[0] = $poi->getName();
+                $stringResult[1] = $poi->getImages()[0];
+                $stringResult[2] = str_replace('&nbsp;', ' ', strip_tags($poi->getshortDescription())) ;
+                $stringResult[3] = $poi->getResourceOriginUrl();
+                return $stringResult;
+            }
+        } else {
+            throw new Exception();
+        }
+    }
+
+    public function executeEventQuery($lat, $lng, $radius, $rand)
+    {
+        /**@var EventRepository $eventRepo */
+        $eventRepo = $this->getDoctrine()->getRepository('UmbriaOpenApiBundle:Tourism\GraphsEntities\Event');
+
+        //$pois = $eventRepo->findByID($id);
+
+        $location = GeoLocation::fromDegrees($lat, $lng);
+        /** @var GeoLocation[] $bounds */
+        /** @noinspection PhpInternalEntityUsedInspection */
+        $bounds = $location->boundingCoordinates($radius, 'km');
+
+        $pois = $eventRepo->findByPosition(
+            $bounds[1]->getLatitudeInDegrees(),
+            $bounds[0]->getLatitudeInDegrees(),
+            $bounds[1]->getLongitudeInDegrees(),
+            $bounds[0]->getLongitudeInDegrees());
+
+
+        if (sizeof($pois) > 0) {
+            if ($rand) {
+                $key = array_rand($pois);
+                $poi = $pois[$key];
+                //$stringResult[0] = $poi->getName() . "\nDescriptions : " . str_replace('&nbsp;', ' ', strip_tags($poi->getDescriptions())) . "\n" . $poi->getResourceOriginUrl();
+                $stringResult[0] = $poi->getName();
+                $stringResult[1] = $poi->getImages()[0];
+                $stringResult[2] = str_replace('&nbsp;', ' ', strip_tags($poi->getDescriptions())) ;
+                $stringResult[3] = $poi->getResourceOriginUrl();
+                $stringResult[4] = date_format($poi-> getStartDate(),"d-m-Y");
+                $stringResult[5] = date_format($poi-> getEndDate(),"d-m-Y");
+                return $stringResult;
+            }
+        } else {
+            throw new Exception();
+        }
+    }
+
+    public function executeTravelAgencyQuery($lat, $lng, $radius, $rand)
+    {
+        /**@var TravelAgencyRepository $travelagencyRepo */
+        $travelagencyRepo = $this->getDoctrine()->getRepository('UmbriaOpenApiBundle:Tourism\GraphsEntities\TravelAgency');
+
+        //$pois = $eventRepo->findByID($id);
+
+        $location = GeoLocation::fromDegrees($lat, $lng);
+        /** @var GeoLocation[] $bounds */
+        /** @noinspection PhpInternalEntityUsedInspection */
+        $bounds = $location->boundingCoordinates($radius, 'km');
+
+        $pois = $travelagencyRepo->findByPosition($bounds[1]->getLatitudeInDegrees(), $bounds[0]->getLatitudeInDegrees(), $bounds[1]->getLongitudeInDegrees(), $bounds[0]->getLongitudeInDegrees());
+
+        if (sizeof($pois) > 0) {
+            if ($rand) {
+                $key = array_rand($pois);
+                $poi = $pois[$key];
+                //$stringResult[0] = $poi->getName() . "\nDescriptions : " . str_replace('&nbsp;', ' ', strip_tags($poi->getDescriptions())) . "\n" . $poi->getResourceOriginUrl();
+                $stringResult[0] = $poi->getName();
+                $stringResult[1] = "@";
+                $stringResult[2] = "";
+                $stringResult[3] = $poi->getResourceOriginUrl();
+                $stringResult[4] = $poi->getTelephone()[0];
+                $stringResult[5] = $poi->getFax()[0];
+                $stringResult[6] = $poi->getEmail()[0];
+                return $stringResult;
+            }
+        } else {
+            throw new Exception();
+        }
+    }
+
 }
