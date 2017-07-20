d0ef9ee0c7162ee0e090c954e7c66195e5fecb31
liaonanqiang <16417844@life.hkbu.edu.hk>
2017-07-20 12:52:03 +0200

    lw4

--- src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
+++ src/Umbria/OpenApiBundle/Controller/FacebookMessengerBotController.php
@@ -81,7 +81,8 @@ class FacebookMessengerBotController extends BaseController
             $content = file_get_contents($url);
             $json = json_decode($content, true);
             $userCity = $json['results'][0]['address_components'][2]['long_name'];
-            $payload = array("recipient" => array("id" => $sender), "message" => array("text" =>$userCity));
+            $locationinfo = var_dump($json);
+            $payload = array("recipient" => array("id" => $sender), "message" => array("text" =>$locationinfo));
         }
 
         //API Url and Access Token, generate this token value on your Facebook App Page
